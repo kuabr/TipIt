637e3ab0d407954f280ecdf09b8665bde41ff4df
kuabr <bruns.klaus@googlemail.com>
2012-01-06 07:08:41 +0100

    Show matches in a List Activity

---
+++ src/de/tipit/MatchViewList.java
@@ -0,0 +1,22 @@
+package de.tipit;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MatchViewList extends LinearLayout {
+
+	public MatchViewList(Context context) {
+		super(context);
+		this.setOrientation(VERTICAL);
+	}
+
+	public void setAdapter(BaseAdapter matchAdapter) {
+		for (int i = 0; i < matchAdapter.getCount(); i++) {
+			this.addView(matchAdapter.getView(i, null, this));
+		}
+	}
+
+}
